3d6a4556ff5325c88401dba7e00800613901803b
yingtu0401@gmail.com <yingtu0401@gmail.com>
2021-08-07 14:33:54 +0800

    adjust mod setting window size

--- Source/RealisticOreGeneration/UI/Window/SettingWindow.cs
+++ Source/RealisticOreGeneration/UI/Window/SettingWindow.cs
@@ -36,43 +36,43 @@ namespace RabiSquare.RealisticOreGeneration
         {
             var ls = new Listing_Standard();
             ls.Begin(inRect);
-            if (ls.ButtonText("Default")) settingModel.SetDefault();
-            Text.Font = GameFont.Medium;
+            Text.Font = GameFont.Small;
             ls.CheckboxLabeled("SrShuffleLumpSize".Translate(), ref settingModel.needShuffleLumpSize,
                 "SrDescriptionShuffleLumpSize".Translate());
-            ls.GapLine(20f);
+            ls.GapLine(15f);
             ls.CheckboxLabeled("SrDisableScanner".Translate(), ref settingModel.disableScanner,
                 "SrDescriptionDisableScanner".Translate());
-            ls.GapLine(20f);
+            ls.GapLine(15f);
             ls.Label($"{"SrVanillaSimilarityPercentage".Translate()}: {settingModel.vanillaPercent.ToStringPercent()}",
                 tooltip: "SrVanillaSimilarityPercentageDesc".Translate());
             settingModel.vanillaPercent = ls.Slider(settingModel.vanillaPercent, 0f, 1f);
-            ls.GapLine(20f);
+            ls.GapLine(15f);
             ls.Label($"{"SrSurfaceMultiplier".Translate()}: {settingModel.surfaceMultiplier}",
                 tooltip: "SrDescriptionSurfaceMultiplier".Translate());
             settingModel.surfaceMultiplier = ls.Slider(settingModel.surfaceMultiplier, 1f, 10f);
-            ls.GapLine(20f);
+            ls.GapLine(15f);
             ls.Label($"{"SrUndergroundMultiplier".Translate()}: {settingModel.undergroundMultiplier}",
                 tooltip: "SrDescriptionUndergroundMultiplier".Translate());
             settingModel.undergroundMultiplier = ls.Slider(settingModel.undergroundMultiplier, 1f, 10f);
-            ls.GapLine(20f);
+            ls.GapLine(15f);
             ls.Label($"{"SrOutpostMapSize".Translate()}: {settingModel.outpostMapSize}",
                 tooltip: "SrDescriptionOutpostMapSize".Translate());
             settingModel.outpostMapSize = Mathf.RoundToInt(ls.Slider(settingModel.outpostMapSize, 100, 300));
-            ls.GapLine(20f);
+            ls.GapLine(15f);
             ls.Label($"{"SrMaxOutpostCount".Translate()}: {settingModel.maxOutpostCount}",
                 tooltip: "SrDescriptionMaxOutpostCount".Translate());
             settingModel.maxOutpostCount = Mathf.RoundToInt(ls.Slider(settingModel.maxOutpostCount, 1, 10));
             if (Prefs.DevMode)
             {
-                ls.GapLine(20f);
+                ls.GapLine(15f);
                 ls.Label($"qMin: {settingModel.qMin}", tooltip: "qMin");
                 settingModel.qMin = ls.Slider(settingModel.qMin, 0.1f, 10f);
-                ls.GapLine(20f);
+                ls.GapLine(15f);
                 ls.Label($"qMax: {settingModel.qMax}", tooltip: "qMax");
-                settingModel.qMax = ls.Slider(settingModel.qMax, 2f, 10f);
+                settingModel.qMax = ls.Slider(settingModel.qMax, 0.1f, 10f);
             }
 
+            if (ls.ButtonText("Default")) settingModel.SetDefault();
             ls.End();
         }
     }
